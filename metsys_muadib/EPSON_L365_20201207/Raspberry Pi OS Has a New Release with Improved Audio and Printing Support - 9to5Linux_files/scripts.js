0676558f81a74d4e03c172755d7060fae9b031b0
r-gregor <gredelonghi@gmail.com>
2024-02-29 17:35:43 +0100

    BIG update 20240229_1735_mdb

--- metsys_muadib/EPSON_L365_20201207/Raspberry Pi OS Has a New Release with Improved Audio and Printing Support - 9to5Linux_files/scripts.js
+++
@@ -1,2 +0,0 @@
-jQuery(document).ready(function(){if(jQuery.isFunction(jQuery.fn.tabs)){jQuery(".bm-tabs-wdt").tabs();}
-if(typeof jQuery.fn.fitVids!=='undefined'){jQuery(".fitvids-video").fitVids();}});
